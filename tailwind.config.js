cfada732e548e23a1daa30cadb1975d2ff872053
IwanIDev <iwan@iwani.dev>
2025-02-07 18:43:20 +0000

    Added colours properly to tailwind, seems they didn't get added for some reason.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     './templates/*.html',
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+    },
   },
   variants: {
     extend: {},
