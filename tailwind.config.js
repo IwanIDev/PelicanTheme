9370e6faaf48158ad42ee59957b326dca83befcc
IwanIDev <iwan@iwani.dev>
2025-02-07 15:19:05 +0000

    Updated structure to hopefully not include unminified tailwind.css.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: [
     './templates/*.html',
   ],
-  darkMode: class, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
